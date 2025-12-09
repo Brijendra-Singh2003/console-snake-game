d2f79e8f28c4eccdc594623ce795af56e30da4a2
Brijendra-Singh2003 <brijendra0369@gmail.com>
2025-12-09 20:50:56 +0530

    smaller board

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ mod game;
 
 #[tokio::main]
 async fn main() {
-    let mut game: Game = Game::new(24, 28);
-    game.set_target_fps(2);
+    let mut game: Game = Game::new(16, 24);
+    game.set_target_fps(4);
 
     game.start().await;
 }
